d8e9228b1c9a5978bb6e4c839fec674886310edf
Karnail Singh Choudhary <pradhankarnail@gmail.com>
2023-09-21 23:20:32 +0530

    viewport
    Signed-off-by: Karnail Singh Choudhary <pradhankarnail@gmail.com>

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,27 @@
+"use client";
 import "./globals.css";
+import React, { useEffect } from "react";
 
 export const metadata = {
   title: "Karnail Singh | Software Engineer",
 };
 
 export default function RootLayout({ children }) {
+  if (typeof window !== "undefined") {
+    window.addEventListener("resize", () => {
+      let vh = window?.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+    let vh = window?.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    }
+  });
+
+
   return (
     <html lang="en">
       <head>
